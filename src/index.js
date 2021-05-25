ccfdba6f0101fafa4da6d96ae64d632a43e35c4c
Andres Link <andres.link@mail.ru>
2021-05-25 19:53:01 +0300

    seach form changed to submit

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
-import './styles.css';
-import './partials/photocard.hbs';
-import { fetchPictures } from './js/apiService.js';
-import galleryList from './partials/galleryList.hbs';
-var debounce = require('lodash.debounce');
+import './styles.css'
+import './partials/photocard.hbs'
+import { fetchPictures } from './js/apiService.js'
+import galleryList from './partials/galleryList.hbs'
+var debounce = require('lodash.debounce')
 
 const refs = {
   searchInput: document.querySelector('.search-form'),
   galleryContainer: document.querySelector('.gallery-list'),
   loadMoreBtn: document.querySelector('.loadMoreBtn'),
-};
+}
 
-refs.searchInput.addEventListener('input', debounce(onSearchInputChange, 500));
-refs.loadMoreBtn.addEventListener('click', loadMorePictures);
+refs.searchInput.addEventListener('submit', onSearchInputChange)
+refs.loadMoreBtn.addEventListener('click', loadMorePictures)
 
-let pageNumber = 1;
-let searchQuery = '';
+let pageNumber = 1
+let searchQuery = ''
 
 function clearContainer() {
-  refs.galleryContainer.innerHTML = '';
-  refs.loadMoreBtn.classList.add('hidden');
+  refs.galleryContainer.innerHTML = ''
+  refs.loadMoreBtn.classList.add('hidden')
 }
 
 function onSearchInputChange(e) {
-  searchQuery = e.target.value;
+  e.preventDefault()
+  searchQuery = e.currentTarget.elements.query.value
   if (searchQuery === '') {
-    clearContainer();
-    return;
+    clearContainer()
+    return
   }
-  clearContainer();
-  renderPictures();
-  refs.loadMoreBtn.classList.remove('hidden');
+  clearContainer()
+  renderPictures()
+  refs.loadMoreBtn.classList.remove('hidden')
+  console.log(searchQuery)
 }
 
 function loadMorePictures() {
-  renderPictures();
+  renderPictures()
 }
 
 function renderPictures() {
   fetchPictures(searchQuery, pageNumber)
-    .then(picture => {
-      pageNumber += 1;
-      const galleryMarkup = galleryList(picture);
-      refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+    .then((picture) => {
+      pageNumber += 1
+      const galleryMarkup = galleryList(picture)
+      refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
       if (pageNumber >= 3) {
         refs.galleryContainer.scrollIntoView({
           behavior: 'smooth',
           block: 'end',
-        });
+        })
       }
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error))
 }
